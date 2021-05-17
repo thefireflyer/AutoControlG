b0d684897e08f4edc550b935ae3b635cf329ce6f
theflyingfire <64284477+theflyingfire@users.noreply.github.com>
2021-05-17 00:16:29 -0700

    v0.2.0
    Added quick links functionality

---
+++ AutoControlG/src/scripts/QuickLinks.gd
@@ -0,0 +1,35 @@
+extends Panel
+
+var links = {
+	
+	"res://assets/64284477.png" : ["https://thefireflyer.herokuapp.com/",
+	 "https://flyingfire.theflyingfire.repl.co/",
+	 "https://thefireflyer.wixsite.com/flyingfire"],
+	
+	"res://assets/Screenshot (78).png" : ["https://itch.io/dashboard"]
+	
+}
+
+func _ready():
+	configure_options(links.keys())
+
+func configure_options(options):
+	var res = []
+	for option in options:
+		#res.append(option)
+		res.append("")
+		#res.append(null)
+		
+		res.append(load(option))
+		res.append(false)
+	$links.items = res
+	
+
+
+func _on_links_item_selected(index):
+	OS.window_minimized = true
+	for link in links.values()[index]:
+		OS.shell_open(link)
+	configure_options(links.keys())
+	
+	
