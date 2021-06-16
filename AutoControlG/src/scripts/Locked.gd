6dd6adf3accd585e830f98750bba069ba51b732d
theflyingfire <64284477+theflyingfire@users.noreply.github.com>
2021-06-16 15:53:33 -0700

    v0.7.0

--- AutoControlG/src/scripts/Locked.gd
+++ AutoControlG/src/scripts/Locked.gd
@@ -4,6 +4,9 @@ var locked = true
 
 func _on_password_text_entered(new_text):
 	if new_text == DataLibrary.password:
+		locked = false
+		name = "Settings"
+		#$settings_button.visible = true
 		for new_environment in DataLibrary.locked["environments"].keys():
 			DataLibrary.environments[new_environment] = DataLibrary.locked["environments"][new_environment]
 			
@@ -12,7 +15,7 @@ func _on_password_text_entered(new_text):
 			DataLibrary.links[new_link] = DataLibrary.locked["links"][new_link]
 		
 		get_parent().get_node("Overview/Environments")._ready()
-		get_parent().current_tab = 0
+		#get_parent().current_tab = 0
 		locked = false
 		#OS.execute("", [], false)
 		#get_tree().quit()
@@ -20,7 +23,9 @@ func _on_password_text_entered(new_text):
 func lock():
 	if locked:
 		return
-	
+	name = "Locked"
+	locked = true
+	#$settings_button.visible = false
 	get_parent().get_node("Overview/Environments")._on_environments_item_selected(DataLibrary.environments.size())
 	
 	for new_environment in DataLibrary.locked["environments"].keys():
@@ -37,3 +42,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_focus_next"):
 		lock()
 	
+	
+	$LockScreen.visible = locked
+	$Settings.visible = not locked
+
