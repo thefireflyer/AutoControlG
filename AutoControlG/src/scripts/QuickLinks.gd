a5c0c4e8d0f55868c41c66d1e68d74b23cfedc49
theflyingfire <64284477+theflyingfire@users.noreply.github.com>
2021-09-23 14:14:39 -0700

    added some support for website icon loading
    Godot doesnt currently seem to have support for loading .ico files thus this update was heavily limited by the supported file formats

--- AutoControlG/src/scripts/QuickLinks.gd
+++ AutoControlG/src/scripts/QuickLinks.gd
@@ -10,8 +10,11 @@ func configure_options(options):
 		#res.append(option)
 		#res.append("link")
 		res.append(null)
-		
-		res.append(load(option))
+		if "res://" in option:
+			res.append(load(option))
+		else:
+			var favicon = yield(get_favicon(str(DataLibrary.user_data["[data]"]["Links"][option][0])), "completed")
+			res.append(favicon)
 		res.append(false)
 	$links.items = res
 	
@@ -25,3 +28,29 @@ func _on_links_item_selected(index):
 	configure_options(DataLibrary.user_data["[data]"]["Links"].keys())
 	
 	
+func get_favicon(url):
+	print(url)
+	print(url.find("://"))
+	print(url.find("/",url.find_last("://")))
+	url = url.substr(0, url.find("/",url.find("://")+3)) + "/favicon.png"#"/fluidicon.png"
+	print(url)
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	var res
+	http_request.request(url)
+	res = yield(http_request, "request_completed")
+	if res[1] != 200:
+		return null
+	#print(typeof(res)==TYPE_ARRAY)
+	var parser = XMLParser.new()
+	print(parser.open_buffer(res[3]))#.get_string_from_utf8())
+	print(parser.is_empty())
+	print(parser.to_string())
+	
+	#print(res)
+	var image = Image.new()
+	var texture = ImageTexture.new()
+	image.load_png_from_buffer(res[3])
+	texture.create_from_image(image)
+	#$Sprite.texture = texture
+	return texture
